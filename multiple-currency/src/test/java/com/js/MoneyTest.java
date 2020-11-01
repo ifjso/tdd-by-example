fde584b32a5d566ba08945c013402f7004d77153
ifjso <ojsboy@hotmail.com>
2020-11-02 00:59:39 +0900

    feat: Dollar 추가

--- multiple-currency/src/test/java/com/js/MoneyTest.java
+++ multiple-currency/src/test/java/com/js/MoneyTest.java
@@ -1,11 +1,13 @@
 package com.js;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
 
   @Test
-  public void testMultiplicationn() {
+  public void testMultiplication() {
     Dollar five = new Dollar(5);
     five.times(2);
     assertEquals(10, five.amount);
