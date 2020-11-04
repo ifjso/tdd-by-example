1305ddd0c6a749e695a599cb0ed32d9dd25b66d6
ifjso <ojsboy@hotmail.com>
2020-11-05 00:53:36 +0900

    feat: plus 추가

--- multiple-currency/src/main/java/com/js/Money.java
+++ multiple-currency/src/main/java/com/js/Money.java
@@ -22,6 +22,10 @@ public class Money {
     return new Money(amount * multiplier, currency);
   }
 
+  Money plus(Money addend) {
+    return new Money(amount + addend.amount, currency);
+  }
+
   String currency() {
     return currency;
   }
