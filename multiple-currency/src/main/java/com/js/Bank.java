37b6a3af59826f2ace988e50b01dd14e138236c6
ifjso <ojsboy@hotmail.com>
2020-11-10 01:09:22 +0900

    fix: Bank에서 환율 가져오도록 수정

--- multiple-currency/src/main/java/com/js/Bank.java
+++ multiple-currency/src/main/java/com/js/Bank.java
@@ -1,12 +1,45 @@
 package com.js;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+  private final Hashtable<Pair, Integer> rates = new Hashtable<>();
 
   Money reduce(Expression source, String to) {
     return source.reduce(this, to);
   }
 
   int rate(String from, String to) {
-    return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    if (from.equals(to)) {
+      return 1;
+    }
+
+    return rates.get(new Pair(from, to));
+  }
+
+  void addRate(String from, String to, int rate) {
+    rates.put(new Pair(from, to), rate);
+  }
+
+  private static class Pair {
+    private final String from;
+    private final String to;
+
+    Pair(String from, String to) {
+      this.from = from;
+      this.to = to;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      Pair pair = (Pair) obj;
+      return from.equals(pair.from) && to.equals(pair.to);
+    }
+
+    @Override
+    public int hashCode() {
+      return 0;
+    }
   }
 }
