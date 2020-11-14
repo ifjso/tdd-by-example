c811215bd6155534baa0e7f39203a11f16398d09
ifjso <ojsboy@hotmail.com>
2020-11-14 17:41:25 +0900

    refactor: Expression times 추상화

--- multiple-currency/src/test/java/com/js/MoneyTest.java
+++ multiple-currency/src/test/java/com/js/MoneyTest.java
@@ -103,6 +103,6 @@ public class MoneyTest {
     bank.addRate("CHF","USD", 2);
     Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
     Money result = bank.reduce(sum, "USD");
-    assertEquals(Money.dollar(10), result);
+    assertEquals(Money.dollar(20), result);
   }
 }
